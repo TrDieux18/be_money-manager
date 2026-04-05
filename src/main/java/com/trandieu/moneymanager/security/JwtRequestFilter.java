1ca131f4462f6bc39ed5b8cdbdce80b9dc5fdb28
Tran Dieu <tracpham2210@gmail.com>
2026-04-05 21:16:39 +0700

    update

--- src/main/java/com/trandieu/moneymanager/security/JwtRequestFilter.java
+++ src/main/java/com/trandieu/moneymanager/security/JwtRequestFilter.java
@@ -1,5 +1,36 @@
 package com.trandieu.moneymanager.security;
 
-public class JwtRequestFilter {
-   
+import java.io.IOException;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.trandieu.moneymanager.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+   private final UserDetailsService userDetailsService;
+   private final JwtUtil jwtUtil;
+
+   @Override
+   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+         throws ServletException, IOException {
+      final String authHeader = request.getHeader("Authorization");
+      String email = null;
+      String jwt = null;
+
+      if(authHeader != null && authHeader.startsWith("Bearer ")) {
+         jwt = authHeader.substring(7);
+         email = jwtUtil.extractEmail(jwt);
+         
+      }
+   }
 }
